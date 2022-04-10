1a7063ceb2d64d8d63e9d7a350e98ac0719c6004
ashawkey <ashawkey1999@gmail.com>
2022-04-10 21:00:01 +0800

    fix packaging

--- nerf/utils.py
+++ nerf/utils.py
@@ -26,11 +26,11 @@ import mcubes
 from rich.console import Console
 from torch_ema import ExponentialMovingAverage
 
-import packaging
+from packaging import version as pver
 
 def custom_meshgrid(*args):
     # ref: https://pytorch.org/docs/stable/generated/torch.meshgrid.html?highlight=meshgrid#torch.meshgrid
-    if packaging.version.parse(torch.__version__) < packaging.version.parse('1.10'):
+    if pver.parse(torch.__version__) < pver.parse('1.10'):
         return torch.meshgrid(*args)
     else:
         return torch.meshgrid(*args, indexing='ij')
